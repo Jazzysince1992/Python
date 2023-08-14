db82ede21fa41f75d7b9f5cb4b8691d7d6ad0126
Jazzysince1992 <Saurabhjaiswaljazzy@gmail.com>
2023-08-14 07:41:31 +0700

    Final guessing game

--- Projects/Guessing Game.py
+++ Projects/Guessing Game.py
@@ -9,23 +9,23 @@ while True:
     if guessed_number.isdigit():
         guessed_number=int(guessed_number)
         if guessed_number>random_number:
-            print("\n You are above the number try guessing smaller number")
+            print("\n You are above the number try guessing smaller number: ")
             list_of_num = []
             for number in list(range(1,guessed_number)):
                 list_of_num.append(number)
-            print("You need to print from the follwoing list of numbers")
+            print("You need to print from the follwoing list of numbers: ")
             print(list_of_num)
         elif guessed_number<random_number:
-            print("\n You are below the number try guessing bigger number.")
+            print("\n You are below the number try guessing bigger number: ")
             list_of_num = []
             for number in list(range(guessed_number+1,max_number+1)):
                 list_of_num.append(number)
-            print("You need to enter from the following list of numbers")
+            print("You need to enter from the following list of numbers:")
             print(list_of_num)
         else:
             print("Congrats you got it")
             break
     else:
-        print("Please enter a valid number")
+        print("Please enter a valid number.")
 
 print("You guessed in",num_of_guesses,"guesses")
