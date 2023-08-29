5e494eb7ae883d31f74191fb14b39935a8a1c4e7
Jazzysince1992 <Saurabhjaiswaljazzy@gmail.com>
2023-08-29 18:50:32 +0700

    End of class with glen on 29/08/23

--- Glen/Dictionaries.py
+++ Glen/Dictionaries.py
@@ -1,56 +1,56 @@
-# person = {
-#     "first_name": "Saurabh",
-#     "last_name": "Prasad",
-#     "age": 31,
-#     "city":"Bangkok"
-# }
+person = {
+    "first_name": "Saurabh",
+    "last_name": "Prasad",
+    "age": 31,
+    "city":"Bangkok"
+}
 
-# print(person["first_name"])
-# print(person["last_name"])
-# print(person["age"])
-# print(person["city"])
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
 
-# fav_num ={
-#     "Jazzy":3,
-#     "Glen":7,
-#     "Elon":9,
-#     "Musk":8
-# }
+fav_num ={
+    "Jazzy":3,
+    "Glen":7,
+    "Elon":9,
+    "Musk":8
+}
 
-# for k,v in fav_num.items():
-#     print(k+" "+str(v))
+for k,v in fav_num.items():
+    print(k+" "+str(v))
 
-# glossary ={
-#     "python":"programming Language",
-#     "variable":"use it to store values",
-#     "for":"for repeating any task",
-#     "ifelse statement":"it is used for conditional functioning"
-# }
+glossary ={
+    "python":"programming Language",
+    "variable":"use it to store values",
+    "for":"for repeating any task",
+    "ifelse statement":"it is used for conditional functioning"
+}
 
-# for term,meaning in glossary.items():
-#     print(term.upper(),": Means this: ",meaning)
+for term,meaning in glossary.items():
+    print(term.upper(),": Means this: ",meaning)
 
-# river_with_country = {
-#     'Nile':'Egypt',
-#     'Chaophraya':'Thailand',
-#     'Thames':'England',
-#     'Ganga':'India'
-# }
-# for river,country in river_with_country.items():
-#     print("The",river,"runs through",country)
-# print("List of rivers are as follows:")
-# for river in river_with_country.keys():
-#     print(river)
-# print("List of countries are as follows:")
-# for country in river_with_country.values():
-#     print(country)
+river_with_country = {
+    'Nile':'Egypt',
+    'Chaophraya':'Thailand',
+    'Thames':'England',
+    'Ganga':'India'
+}
+for river,country in river_with_country.items():
+    print("The",river,"runs through",country)
+print("List of rivers are as follows:")
+for river in river_with_country.keys():
+    print(river)
+print("List of countries are as follows:")
+for country in river_with_country.values():
+    print(country)
 
-# favorite_languages = {
-#     'jen': 'python',
-#     'sarah': 'c',
-#     'edward': 'ruby',
-#     'phil': 'python',
-#     }
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
 
 #6-6 page number 141.
 
@@ -77,34 +77,34 @@ person3 = {
 
 persons = [person1,person2,person3]
 
-# for person in persons:
-#     print(person["first_name"],person["last_name"]," is full name of the person")
-#     print(person["first_name"],person["last_name"], "is",str(person["age"]),"year old.")
-#     print("And the person lives in",person["city"])
-#     print("::::::::::::::::::::::::::::::::::::::::::::::::::")
+for person in persons:
+    print(person["first_name"],person["last_name"]," is full name of the person")
+    print(person["first_name"],person["last_name"], "is",str(person["age"]),"year old.")
+    print("And the person lives in",person["city"])
+    print("::::::::::::::::::::::::::::::::::::::::::::::::::")
 
-# for person in persons:
-#     print(person["first_name"],person["last_name"],"is full name of the person.","\nThis person is",person["age"],"year old","\nAnd lives in",person["city"])
-#     print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+for person in persons:
+    print(person["first_name"],person["last_name"],"is full name of the person.","\nThis person is",person["age"],"year old","\nAnd lives in",person["city"])
+    print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
 
-# pipo={
-#     "name":"pipo",
-#     "kind":"Dog",
-#     "owner":"Jazzy"
-# }
-# moji={
-#     "name":"moji",
-#     "kind":"Cat",
-#     "owner":"Aiw"
-# }
-# larry={
-#     "name":"larry",
-#     "kind":"Cat",
-#     "owner":"Jazzy"
-# }
-# pets = [pipo,moji,larry]
-# for pet in pets:
-#     print(pet["name"].title(),"is a",pet["kind"].lower(),"owned by",pet["owner"])
+pipo={
+    "name":"pipo",
+    "kind":"Dog",
+    "owner":"Jazzy"
+}
+moji={
+    "name":"moji",
+    "kind":"Cat",
+    "owner":"Aiw"
+}
+larry={
+    "name":"larry",
+    "kind":"Cat",
+    "owner":"Jazzy"
+}
+pets = [pipo,moji,larry]
+for pet in pets:
+    print(pet["name"].title(),"is a",pet["kind"].lower(),"owned by",pet["owner"])
 
 favorite_places = {
     "Jazzy":["Bangkok","Ayutthaya","Kanchanburi","Pattaya"],
@@ -112,15 +112,15 @@ favorite_places = {
     "Elon":["America","Thailand","Phuket","India"],
     "Mark":["Thailand","India"]
 }
-# print("------------------------------------------")
-# for person,places in favorite_places.items():
-#     print(person,"loves following places alot:")
-#     count=1
-#     for place in places:
-#         print(count,place)
-#         count+=1
-#     print("Person likes",count-1,"places")
-#     print("------------------------------------------")
+print("------------------------------------------")
+for person,places in favorite_places.items():
+    print(person,"loves following places alot:")
+    count=1
+    for place in places:
+        print(count,place)
+        count+=1
+    print("Person likes",count-1,"places")
+    print("------------------------------------------")
 
 favorite_numbers = {
     "Jazzy":[1,3,5,7,9],
@@ -129,10 +129,10 @@ favorite_numbers = {
     "Mark":[0,5,10,15,20]
 }
 
-# for name,numbers in favorite_numbers.items():
-#     print(name,"likes following numbers:")
-#     for number in numbers:
-#         print("\t",number)
+for name,numbers in favorite_numbers.items():
+    print(name,"likes following numbers:")
+    for number in numbers:
+        print("\t",number)
 
 cities = {
     "Bangkok":{
---
+++ Glen/User Inpur and while loops.py
@@ -0,0 +1,15 @@
+# car_model = input("What car model you want to rent? ")
+# print("Let me see if I can find you a",car_model,".")
+
+# number_of_guests = int(input("Please enter the number of people you have: "))
+# print(type(number_of_guests))
+# if number_of_guests>8:
+#     print("You will have to wait for a table")
+# else:
+#     print("Your table is ready")
+
+number = int(input("please enter a number: "))
+if number%10 == 0:
+    print("Yes the number is multiple of 10")
+else:
+    print("No number is not multiple of 10")
