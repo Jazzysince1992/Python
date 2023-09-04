e13d8cfdbb7380a81c2b33d7d5ff6a9a7058a1b6
Jazzysince1992 <Saurabhjaiswaljazzy@gmail.com>
2023-09-04 09:02:43 +0700

    End of class on saturday

---
+++ Projects/Data Entry Form /Data Entry Form.py
@@ -0,0 +1,7 @@
+import tkinter
+window = tkinter.Tk()
+window.title("Data Entry Form")
+frame = tkinter.Frame(window)
+frame.pack()
+user_info_frame = tkinter.LabelFrame(frame,text="User Information")
+window.mainloop()
